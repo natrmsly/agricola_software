5ecdba0f03efafebcf6d56a39efa9331842ff0a9
natrmsly <ramosnatally94@gmail.com>
2025-07-18 14:53:53 +0000

    atidade começo(ta errada)

---
+++ cadastrar_gastos.py
@@ -0,0 +1,23 @@
+
+custo_tomate = {'arr': 5.0,'mudas': 100.0, 'ins': 30.0, 'maodobra': 60.0, 'eng': 600.0}
+bruto_tomate = [3.00, 500]
+
+custo_abacaxi = {'arr': 5.0,'mudas': 110.0, 'ins': 40.0, 'maodobra': 50.0, 'eng': 600.0}
+bruto_abacaxi = [5.00, 600]
+
+custo_batata = {'arr': 5.0,'mudas': 90.0, 'ins': 60.0, 'maodobra': 60.0, 'eng': 610.0}
+bruto_batata = [4.00, 500]
+
+custo_uva = {'arr': 5.0,'mudas': 100.0, 'ins': 65.0, 'maodobra': 40.0, 'eng': 600.0}
+bruto_uva = [2.00, 700]
+
+custo_cenoura = {'arr': 5.0,'mudas': 120.0, 'ins': 70.0, 'maodobra': 100.0, 'eng': 605.0}
+bruto_cenoura = [7.00, 580]
+
+qnt_hecta = int(input("Digite a quantidade de hectares: "))
+produto = input("Digite o produto plantado: ")
+
+if produto == "tomate":
+    custo_tomate * qnt_hecta
+    
+
